60d03425cfcece6b48e5312ea0985f746b94a786
tanish-sd26 <mauryatanisha824@gmail.com>
2026-07-11 01:04:45 +0530

    feat(app): configure root application module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ClientsModule } from './clients/clients.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, ClientsModule, ProjectsModule, TasksModule],
 })
 export class AppModule {}
